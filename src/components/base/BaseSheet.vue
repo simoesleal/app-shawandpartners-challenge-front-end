4d20db3a9daa47305877d9103052b6331785b0c7
Antonio Simões <simoeslealneto@gmail.com>
2021-02-06 18:02:53 -0300

    Added lodash 4.17.20 to the project, implemented auto import of base components, implemented the BaseButton, BaseInput and BaseSheet components, implemented the first and second row of the home.

---
+++ src/components/base/BaseSheet.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-sheet
+    v-bind="$attrs"
+    :color="color"
+    :dark="dark"
+    :elevation="elevation"
+    :height="height"
+    :light="light"
+    :max-height="maxHeight"
+    :max-width="maxWidth"
+    :min-height="minHeight"
+    :min-width="minWidth"
+    :outlined="outlined"
+    :rounded="rounded"
+    :shaped="shaped"
+    :tag="tag"
+    :tile="tile"
+    :width="width"
+  >
+    <slot></slot>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "BaseSheet",
+
+  inheritAttrs: false,
+
+  props: {
+    /** @description Applies specified color to the control. */
+    color: {
+      type: String,
+      default: undefined,
+    },
+
+    /** @description Applies the dark theme variant to the component. */
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Designates an elevation applied to the component between 0 and 24 */
+    elevation: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Sets the height for the component. */
+    height: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Applies the light theme variant to the component. */
+    light: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Sets the maximum height for the component. */
+    maxHeight: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Sets the maximum width for the component. */
+    maxWidth: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Sets the minimum height for the component. */
+    minHeight: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Sets the minimum width for the component. */
+    minWidth: {
+      type: [Number, String],
+      default: undefined,
+    },
+
+    /** @description Applies the v-ripple directive. */
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Applies a large border radius on the button. */
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Applies a large border radius on the top left and bottom right of the card. */
+    shaped: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Specify a custom tag used on the root element. */
+    tag: {
+      type: String,
+      default: "div",
+    },
+
+    /** @description Removes the component's border-radius. */
+    tile: {
+      type: Boolean,
+      default: false,
+    },
+
+    /** @description Sets the width for the component. */
+    width: {
+      type: [Number, String],
+      default: undefined,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,172 @@
 <template>
-  <div id="dashboard"></div>
+  <div id="dashboard">
+    <v-container class="mt-3">
+      <v-row no-gutters>
+        <v-col cols="10">
+          <BaseInput
+            v-model.trim.lazy="searchField"
+            :backgroundColor="'white'"
+            :color="'#aac173'"
+            :dense="true"
+            :label="'Search user:'"
+            :placeholder="'simoesleal'"
+            :rounded="true"
+          >
+          </BaseInput>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="1">
+          <BaseButton :elevation="1" :fab="true" xSmall>
+            <v-icon :color="'#aac173'">mdi-magnify</v-icon>
+          </BaseButton>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="wrapper" :style="`height: ${300}px;`">
+      <div
+        v-for="(user, index) in users"
+        :key="index"
+        class="ml-2 mr-8"
+        :style="`min-width: ${200}px;`"
+      >
+        <BaseSheet
+          :color="'white'"
+          :dark="false"
+          :elevation="6"
+          :height="'250px'"
+          :light="true"
+          :outlined="false"
+          :rounded="false"
+          :shaped="false"
+          :tile="false"
+          :width="'200px'"
+        >
+          <div>
+            <v-row no-gutters class="pa-2">
+              <v-col cols="12" class="mb-3">
+                <v-img
+                  class="mx-auto"
+                  :style="'border-radius: 100%;'"
+                  :height="'100px'"
+                  :width="'100px'"
+                  :src="user.avatar_url"
+                ></v-img>
+              </v-col>
+              <v-col cols="6" class="d-flex">
+                <span class="text-uppercase mr-3" :style="'color: #AAC173;'"
+                  >ID
+                </span>
+              </v-col>
+              <v-col cols="6" class="d-flex justify-start">
+                <span>{{ user.id }}</span>
+              </v-col>
+              <v-col cols="6" class="d-flex">
+                <span class="text-uppercase mr-3" :style="'color: #AAC173;'"
+                  >Login
+                </span>
+              </v-col>
+              <v-col cols="6" class="d-flex justify-start">
+                <span>{{ user.login }}</span>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col class="d-flex justify-center">
+                <BaseButton
+                  :color="'#5C6C54'"
+                  dark
+                  :elevation="0"
+                  :rounded="true"
+                  small
+                >
+                  Details
+                </BaseButton>
+              </v-col>
+            </v-row>
+          </div>
+        </BaseSheet>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Dashboard",
+
+  data: () => ({
+    h: 100,
+    mw: 150,
+    searchField: null,
+    users: [
+      {
+        login: "technoweenie",
+        id: 21,
+        node_id: "MDQ6VXNlcjIx",
+        avatar_url: "https://avatars.githubusercontent.com/u/21?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/technoweenie",
+        html_url: "https://github.com/technoweenie",
+        followers_url: "https://api.github.com/users/technoweenie/followers",
+        following_url:
+          "https://api.github.com/users/technoweenie/following{/other_user}",
+        gists_url: "https://api.github.com/users/technoweenie/gists{/gist_id}",
+        starred_url:
+          "https://api.github.com/users/technoweenie/starred{/owner}{/repo}",
+        subscriptions_url:
+          "https://api.github.com/users/technoweenie/subscriptions",
+        organizations_url: "https://api.github.com/users/technoweenie/orgs",
+        repos_url: "https://api.github.com/users/technoweenie/repos",
+        events_url:
+          "https://api.github.com/users/technoweenie/events{/privacy}",
+        received_events_url:
+          "https://api.github.com/users/technoweenie/received_events",
+        type: "User",
+        site_admin: false,
+      },
+      {
+        login: "macournoyer",
+        id: 22,
+        node_id: "MDQ6VXNlcjIy",
+        avatar_url: "https://avatars.githubusercontent.com/u/22?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/macournoyer",
+        html_url: "https://github.com/macournoyer",
+        followers_url: "https://api.github.com/users/macournoyer/followers",
+        following_url:
+          "https://api.github.com/users/macournoyer/following{/other_user}",
+        gists_url: "https://api.github.com/users/macournoyer/gists{/gist_id}",
+        starred_url:
+          "https://api.github.com/users/macournoyer/starred{/owner}{/repo}",
+        subscriptions_url:
+          "https://api.github.com/users/macournoyer/subscriptions",
+        organizations_url: "https://api.github.com/users/macournoyer/orgs",
+        repos_url: "https://api.github.com/users/macournoyer/repos",
+        events_url: "https://api.github.com/users/macournoyer/events{/privacy}",
+        received_events_url:
+          "https://api.github.com/users/macournoyer/received_events",
+        type: "User",
+        site_admin: false,
+      },
+    ],
+  }),
 };
 </script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.wrapper::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.wrapper {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
